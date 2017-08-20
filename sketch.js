01c817784072c5511707545cadcfba669f7ef2db
bodya17 <levantovychbogdan@gmail.com>
2017-08-20 12:58:20 +0300

    add better check for neigbors

--- sketch.js
+++ sketch.js
@@ -1,12 +1,15 @@
 var cellWidth
 var cellHeight
-var cellCount = 8
+var cellCount = 40
 var cells = []
-var colorPicker = document.querySelector('input[type=color]')
+// var colorPicker = document.querySelector('input[type=color]')
 var bombs = []
 var i
 var j
 var playButton
+var autoButton
+var rateSlider
+var auto = false
 
 class Cell {
   constructor(x, y) {
@@ -14,14 +17,19 @@ class Cell {
     this.y = y
     this.alive = false
   }
+
+  toggle() {
+    this.alive ? this.dead() : this.live()
+  }
+
   live() {
     this.alive = true
     textSize(32)
    
     fill(200, 100, 150, 255)
     rect(this.x * cellWidth, this.y * cellHeight, cellWidth, cellHeight)
-    fill(255)
-    text(`${this.x} ${this.y}`, this.x * cellWidth + cellWidth / 4, this.y * cellHeight + cellHeight / 2)
+    // fill(255)
+    // text(`${this.x} ${this.y}`, this.x * cellWidth + cellWidth / 4, this.y * cellHeight + cellHeight / 2)
   }
 
   dead() {
@@ -47,29 +55,56 @@ function drawBoard() {
 }
 
 function setup() {
-  createCanvas(600, 600)
+  createCanvas(800, 800)
   drawBoard()
   playButton = createButton('play')
+  autoButton = createButton('auto')
   playButton.mousePressed(play)
+  autoButton.mousePressed(() => auto = !auto)
+  rateSlider = createSlider(0, 60, 5, 1)
 }
 
 function getAliveNeighborsCount(i, j, previousState) {
-  var aboveLeft = (i - 1) * cellCount + (j - 1)
-  var aboveCenter = (i - 1) * cellCount + j
-  var aboveRight = (i - 1) * cellCount + (j + 1)
+  var aboveLeft
+  var aboveCenter
+  var aboveRight
+  var left
+  var right
+  var bottomLeft 
+  var bottomCenter
+  var bottomRight
 
-  var left = i * cellCount + (j - 1)
-  var right = i * cellCount + (j + 1)
+  if (i > 0 && j > 0)
+    aboveLeft = (i - 1) * cellCount + (j - 1)
+  
+  if (i > 0)
+    aboveCenter = (i - 1) * cellCount + j
+  
+  if (i > 0 && j < cellCount - 1)
+    aboveRight = (i - 1) * cellCount + (j + 1)
 
-  var bottomLeft = (i + 1) * cellCount + (j - 1)
-  var bottomCenter = (i + 1) * cellCount + j
-  var bottomRight = (i + 1) * cellCount + (j + 1)
+  if (j > 0)
+    left = i * cellCount + (j - 1)
+  
+  if (j < cellCount - 1)
+    right = i * cellCount + (j + 1)
+
+  if (i < cellCount - 1 && j > 0)
+    bottomLeft = (i + 1) * cellCount + (j - 1)
+  
+  if (i < cellCount - 1)
+    bottomCenter = (i + 1) * cellCount + j
+  
+  if (i < cellCount - 1 && j < cellCount - 1)
+    bottomRight = (i + 1) * cellCount + (j + 1)
 
   var neighbors = [
     aboveLeft, aboveCenter,  aboveRight,
-    left,                 ,  right,
+    left,                         right,
     bottomLeft, bottomCenter, bottomRight
-  ]
+  ].filter(n => n !== undefined)
+
+  // console.log(`neighbors: (${i} - ${j})`, neighbors)
   var neighborsCount = neighbors.reduce(function checkNeighbor(aliveTotal, neighbor) {
     if (previousState[neighbor] && previousState[neighbor].alive)
       return aliveTotal + 1
@@ -85,7 +120,7 @@ function play() {
   for (var j = 0; j < cellCount; j++) {
     for (var i = 0; i < cellCount; i++) {
       var neighborsCount = getAliveNeighborsCount(i, j, previousState)
-      console.log(`(${i}, ${j}) = ${neighborsCount}`)
+      // console.log(`(${i}, ${j}) = ${neighborsCount}`)
 
       if (neighborsCount > 3 || neighborsCount < 2) {
         cells[i * cellCount + j].dead()
@@ -97,7 +132,11 @@ function play() {
 }
 
 function draw() {
-
+  // console.log(rateSlider.value())
+  frameRate(+rateSlider.value())
+  if (auto)
+    play()
+  // play()
 }
 
 function mousePressed(e) {
@@ -106,7 +145,7 @@ function mousePressed(e) {
   if (xClicked < width && yClicked < height) {
     i = floor(xClicked / cellWidth) 
     j = floor(yClicked / cellHeight)
-    cells[i * cellCount + j].live()
+    cells[i * cellCount + j].toggle()
   }
   
   // colorPicker.click()
